83df789fa23bb48c0cdff165143fa0abf55ec6a4
efriedla <efriedla20852@gmail.com>
2018-02-14 17:06:29 -0800

    search bar grab value but state not changed

--- src/containers/SearchBar.js
+++ src/containers/SearchBar.js
@@ -1,10 +1,32 @@
 import React, {Component} from "react";
 
 export default class SearchBar extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = { term: ' ' };
+    }
+    onInputChange(event){
+        console.log(event.target.value);
+    }
     render() {
         return(
-            <form className="form-inline">
-                <input type="search" placeholder="search" />
+            <form >
+                <div className="input-group">
+                    <input 
+                        type="search" 
+                        className="form-control" 
+                        placeholder="Search Forcasts" 
+                        aria-describedby="basic-addon2"
+                        value={this.state.term}
+                        onChange={this.onInputChange}
+                        />
+                    <span 
+                        type="submit"
+                        className="input-group-addon btn btn-primary" 
+                        id="basic-addon2">Submit
+                    </span>
+                </div>           
             </form>
         );
     }

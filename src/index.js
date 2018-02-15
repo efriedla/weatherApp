ac365bcc7792d40e9800e154da510e14becbfcdc
efriedla <efriedla20852@gmail.com>
2018-02-14 18:12:07 -0800

    imported redux-promise in index.js

--- src/index.js
+++ src/index.js
@@ -6,10 +6,11 @@ import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reducers from './reducers';
+import ReduxPromise from 'redux-promise';
 
 // ReactDOM.render(<App />, document.getElementById('root'));
 // registerServiceWorker();
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>

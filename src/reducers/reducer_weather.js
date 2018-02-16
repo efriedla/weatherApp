243326a7579aa943d97ea2018ee866bf0c9b1bb1
efriedla <efriedla20852@gmail.com>
2018-02-16 13:24:41 -0800

    setting up payload to read data

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,4 +1,16 @@
-export default function(state = null, action){
-    console.log("action: " + action);
+/**
+ * importing Fetch_Weather from action/mainAction
+ * @param {*} state  is an array
+ * @param {*} action  is what we get ack from the api
+ */
+import { Fetch_Weather } from "../actions/mainAction";
+export default function(state = [], action){
+    switch(action.type){
+        case 'Fetch_Weather':
+        // handle payload
+        // return state.concat([action.payload.data]);
+        return [ action.payload.data, ...state ]; // [ city, city, city ]
+    }
+    // console.log("action: " + action);
     return state;
 }

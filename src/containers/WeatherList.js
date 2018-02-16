67ac77ba2ef12bacda969a22cd1d02dbd7585791
efriedla <efriedla20852@gmail.com>
2018-02-16 13:41:01 -0800

    outline of bootstrap table

--- src/containers/WeatherList.js
+++ src/containers/WeatherList.js
@@ -15,23 +15,12 @@ export default class WeatherList extends Component{
                 </thead>
                 <tbody>
                     <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
+                    <th scope="row">Seattle</th>
+                    <td>Rainy</td>
+                    <td>43</td>
+                    <td>40</td>
                     </tr>
-                    <tr>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                    </tr>
-                    <tr>
-                    <th scope="row">3</th>
-                    <td>Larry</td>
-                    <td>the Bird</td>
-                    <td>@twitter</td>
-                    </tr>
+                 
                 </tbody>
             </table>
             );

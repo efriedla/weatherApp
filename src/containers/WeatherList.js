0b46dfbab33d0fc07a6b6272486595d8686a1e19
efriedla <efriedla20852@gmail.com>
2018-02-16 13:47:18 -0800

    WeatherList map es6

--- src/containers/WeatherList.js
+++ src/containers/WeatherList.js
@@ -28,6 +28,6 @@ export default class WeatherList extends Component{
         }
 }
 
-function mapStateToProps(state){
-    return { weather: state.weather };
+function mapStateToProps({weather}){
+    return { weather: weather };
 }

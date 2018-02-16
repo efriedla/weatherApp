65e06812611cd5ecbc0fd3c281be7e63a0ff5fc9
efriedla <efriedla20852@gmail.com>
2018-02-16 13:37:00 -0800

    updated readme

--- src/actions/mainAction.js
+++ src/actions/mainAction.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 // import api_key from './API_KEY';
 
-const api_key = "94983fb11c548d0a5c3229b04de38637";
 const Root_URL = `http://api.openweathermap.org/data/2.5/forecast?&appid=${api_key}` ;
 export const Fetch_Weather = "Fetch the Weather Please";
 export function fetchWeather(city){
